96de7749568a999524bdb9c0dd6f3cabe7047450
Mahmud-cse <mahmudhasan555666@gmail.com>
2021-10-29 15:56:00 +0600

    private Route

--- src/App.js
+++ src/App.js
@@ -13,38 +13,39 @@ import Footer from './components/Footer/Footer';
 import AuthProvider from './components/Context/AuthProvider';
 import MyOrders from './components/MyOrders/MyOrders';
 import PlaceOrder from './components/PlaceOrder/PlaceOrder';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 function App() {
   return (
     <div>
       {/* context with router */}
-    <AuthProvider>
-    <Router>
-      <NavBar />
-        <Switch>
-         <Route exact path="/">
-           <Home />
-         </Route>
-         <Route exact path="/home">
-           <Home />
-         </Route>
-         <Route exact path="/orders">
-           <MyOrders />
-         </Route>
-         <Route exact path="/placeOrder/:id">
-           <PlaceOrder />
-         </Route>
-         <Route exact path="/login">
-           <Login />
-         </Route>
-         {/* <Route path="*">
+      <AuthProvider>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <PrivateRoute exact path="/orders">
+              <MyOrders />
+            </PrivateRoute>
+            <PrivateRoute exact path="/placeOrder/:id">
+              <PlaceOrder />
+            </PrivateRoute>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            {/* <Route path="*">
            <NotFound />
          </Route> */}
-       </Switch>
-       <Footer />
-    </Router>
-    </AuthProvider>
-   </div> 
+          </Switch>
+          <Footer />
+        </Router>
+      </AuthProvider>
+    </div>
   );
 }
 
---
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+import { Redirect, Route } from 'react-router';
+import useAuth from '../hooks/useAuth';
+
+const PrivateRoute = ({ children, ...rest }) => {
+    const { user, isLoading } = useAuth();
+    if (isLoading) {
+        return <Spinner style={{ marginLeft: "50%" }} animation="border" variant="primary" />
+    }
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                user.displayName || user.email ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                            state: { from: location }
+                        }}
+                    />
+                )
+            }
+        />
+    );
+};
+export default PrivateRoute;
